f401a6498fe8d6650508c446f8cf2086a36c1397
Hanyo Shunta <b1018162.fun@gmail.com>
2020-05-15 17:39:27 +0900

    [add]Taskの追加機能

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
 
   return (
     <div>
-       <TaskInput />
+       <TaskInput　tasks={tasks} setTasks={setTasks} />
       <TaskList tasks={tasks} />
     </div>
   )
