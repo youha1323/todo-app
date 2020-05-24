e7e4e3c5187b325aa2a1b26a19cefea443b23c25
Hanyo Shunta <b1018162.fun@gmail.com>
2020-05-25 08:46:42 +0900

    [add]:Userに属性を持たせる

---
+++ src/components/User.tsx
@@ -0,0 +1,15 @@
+import { ImgHTMLAttributes } from "react";
+
+export default class User  {
+  //プロパティとして、氏名,称賛回数,称賛された回数を持つ
+  name: string;
+  clap: number;
+  applause: number;
+  
+  constructor(name: string) {
+    this.name=name;//name は後から変更できるように
+    this.clap=100;
+    this.applause=0;
+  }
+
+}
