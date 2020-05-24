c97df5a9ffac0ffea6d769b74cd5f405b745a41b
Hanyo Shunta <b1018162.fun@gmail.com>
2020-05-25 02:23:35 +0900

    [add]:Postの文字数制限と投稿時刻表示

--- src/components/PostInput.tsx
+++ src/components/PostInput.tsx
@@ -1,32 +1,47 @@
 import React,{ useState } from 'react';
 import { Post } from '../Types';
+import formatDate from './formatDate';
 
 type Props = {
   posts: Post[];
   setPosts: React.Dispatch<React.SetStateAction<Post[]>>
 }
 
+enum DateFormat {
+  YY_MM_DD_dd_HH_mm = 'YYYY/MM/DD(dd) HH:mm',
+}
+
 //{posts, setPosts} は分割代入っていう書き方で、propsの数が多くなるのを防ぐことができる
 const PostInput: React.FC<Props> = ({posts, setPosts}) => {
   const [inputTitle, setInputTitle] = useState('');
+  const [inputTime, setInputTime] = useState('');
   const [count, setCount] = useState(posts.length + 1);
+  const today = formatDate(new Date(), DateFormat.YY_MM_DD_dd_HH_mm);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     /* e.turget.valueでinputのvalueを取得できる */
     setInputTitle(e.target.value)
+    setInputTime(today)
   }
 
   const hundleSubmit = () => {
-    setCount(count + 1);
 
-    const newPost : Post = {
-      id: count,
-      title: inputTitle,
-      done: false
-    }
+    if(inputTitle.length >= 5){
+      setCount(count + 1);
+
+      const newPost : Post = {
+        id: count,
+        title: inputTitle+inputTime ,
+        done: false,
+      }
 
       setPosts([newPost, ...posts]);//作成したnewPostsを追加する
-      setInputTitle('');//Formをclearの認識？
+      setInputTitle('');//Formをclear
+
+
+    }else{
+      alert("5文字以上入力してください");
+    }
   }
 
   return (
@@ -38,7 +53,7 @@ const PostInput: React.FC<Props> = ({posts, setPosts}) => {
         onChange={handleInputChange}
         placeholder="皆に紹介してください！"
         />
-        <button　onClick={hundleSubmit} className="btn is-primary">追加</button>
+        <button　onClick={hundleSubmit} className="btn is-primary">投稿</button>
       </div>
 
     </div>
