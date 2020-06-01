d24c5fff2bc98507683cb83ad87f818b2904e1c9
Shunta Hanyo <b1018162.fun@gmail.com>
2020-06-01 16:47:13 +0900

    [add]：clapfeature

--- src/components/Area/UserArea.tsx
+++ src/components/Area/UserArea.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GridArea from 'src/styles/GridArea';
 import UserInfo from '../UserInfo';
+import Clapbtn from '../Clapbtn';
 
 type Props = {
   area: string;
@@ -13,6 +14,7 @@ const UserArea: React.FC<Props> = (props) => {
     <div>
       <GridArea area = {area}>
         <UserInfo />
+        <Clapbtn />
       </GridArea>
     </div>
   );
---
+++ src/components/Clapbtn.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Clap from '../img/Clap.png';
+
+const Clapbtn: React.FC = () => {
+  const [clap, setClap] = useState(0);
+
+  const hundleClick = () => {
+    setClap(clap + 1);
+  };
+  
+  var img = new Image();
+  img.src = 'Clap.png';
+  
+  return(
+    <div>
+      <button
+         onClick={hundleClick}>
+         <img src={Clap} alt="clap Image" width= "30" height= "30"/>
+      </button>
+    </div>
+  );
+}
+
+export default Clapbtn;
